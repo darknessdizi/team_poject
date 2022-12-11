db06b2a866f145da1e92cca572278d4a4846cc75
Remezov Dmitriy <darknessdizi@gmail.com>
2022-12-12 06:35:38 +0700

    Changes to main: added the main() function

--- main.py
+++ main.py
@@ -4,12 +4,35 @@ import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
 
-def write_msg(user_id, message):
+def write_msg(user_id: str, message: str) -> None:
     vk.method('messages.send', {
         'user_id': user_id, 
         'message': message, 
         'random_id': randint(0, 1000)
         })
+
+def main():
+    # Основной цикл
+    for event in longpoll.listen():
+
+        # Если пришло новое сообщение
+        if event.type == VkEventType.MESSAGE_NEW:
+        
+            # Если оно имеет метку для меня(то есть бота)
+            if event.to_me:
+            
+                # Сообщение от пользователя
+                request = event.text
+                
+                # Каменная логика ответа
+                if request == "привет":
+                    write_msg(event.user_id, "Хай")
+                elif request == "пока":
+                    write_msg(event.user_id, "Пока((")
+                elif request == "как дела":
+                    write_msg(event.user_id, "не плохо")
+                else:
+                    write_msg(event.user_id, "Не поняла вашего ответа...")
 
 
 # Авторизуемся как сообщество
@@ -18,26 +41,8 @@ vk = vk_api.VkApi(token=token_vk_community)
 # Работа с сообщениями
 longpoll = VkLongPoll(vk)
 
-# Основной цикл
-for event in longpoll.listen():
 
-    # Если пришло новое сообщение
-    if event.type == VkEventType.MESSAGE_NEW:
-    
-        # Если оно имеет метку для меня(то есть бота)
-        if event.to_me:
-        
-            # Сообщение от пользователя
-            request = event.text
-            
-            # Каменная логика ответа
-            if request == "привет":
-                write_msg(event.user_id, "Хай")
-            elif request == "пока":
-                write_msg(event.user_id, "Пока((")
-            elif request == "как дела":
-                write_msg(event.user_id, "не плохо")
-            else:
-                write_msg(event.user_id, "Не поняла вашего ответа...")
+if __name__ == '__main__':
+    main()
             
             
