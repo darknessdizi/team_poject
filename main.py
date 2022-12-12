7dd86ecc8e063db7e1ecbc675bec328719625cc9
Remezov Dmitriy <darknessdizi@gmail.com>
2022-12-12 07:23:21 +0700

    Changes to main: added the send_photo and add_photos functions

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ def write_msg(user_id: str, message: str) -> None:
         'random_id': randint(0, 1000)
         })
 
+def send_photo(user_id: str, attachment: list) -> None:
+    for element in attachment:
+        vk.method('messages.send', {
+            'user_id': user_id, 
+            'attachment': element,
+            'random_id': randint(0, 1000)
+            })
+
+def add_photos(list_photos: list) -> list:
+    attachment_list = []
+    uploader = vk_api.VkUpload(vk)
+    for element in list_photos:
+        img = uploader.photo_messages(element)
+        media_id = str(img[0]['id'])
+        owner_id = str(img[0]['owner_id'])
+        attachment_list.append(f'photo{owner_id}_{media_id}')
+    return attachment_list
+
 def main():
     # Основной цикл
     for event in longpoll.listen():
@@ -31,6 +49,10 @@ def main():
                     write_msg(event.user_id, "Пока((")
                 elif request == "как дела":
                     write_msg(event.user_id, "не плохо")
+                elif request == "фото":
+                    my_list = ['test_photo\kot.jpg', 'test_photo\kot2.jpg', 'test_photo\kot3.jpg']
+                    attachment = add_photos(my_list)
+                    send_photo(event.user_id, attachment)
                 else:
                     write_msg(event.user_id, "Не поняла вашего ответа...")
 
