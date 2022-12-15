f473dc70a31497c1d3614d1510ffd1c99d03b7f8
Mariya Zamyatina <maria.net@mail.ru>
2022-12-16 00:07:41 +0700

    requests_to_vk.py

--- requests_to_vk.py
+++ requests_to_vk.py
@@ -6,7 +6,6 @@ import os
 import datetime
 import time
 
-
 load_dotenv(".env")
 
 
@@ -20,7 +19,9 @@ class VK:
         return {'Content-Type': 'application/json', 'Authorization': f'OAuth {self.access_token}'}
 
     def get_user(self, user_id):
+
         """ возвращает инф-ию о пользователе"""
+
         url = "https://api.vk.com/method/users.get"
         headers = self.get_headers()
         params = {"user_ids": user_id,
@@ -34,7 +35,8 @@ class VK:
         user_info['user_name'] = name
         if res.json().get('response')[0].get("city"):
             city = res.json().get('response')[0].get("city").get("title")
-        else: city = ""
+        else:
+            city = ""
         user_info['city'] = city
         if res.json().get('response')[0].get("sex"):
             sex = res.json().get('response')[0].get("sex")
@@ -49,24 +51,25 @@ class VK:
         return user_info
 
     def get_users(self, city, sex, age):
+        """ Получаем список пользователей"""
         url = "https://api.vk.com/method/users.search"
         headers = self.get_headers()
         if "-" in age:
             age_from, age_to = age.split("-")
             params = {'fields': "first_name, last_name, bdate, sex",
-                  'q': city,
-                  'count': 1000,
-                  'offset': 1,
-                  'age_from': age_from,
-                  'age_to': age_to,
-                  'sex': sex
-                  }
+                      'q': city,
+                      'count': 1000,
+                      'offset': 1,
+                      'age_from': age_from,
+                      'age_to': age_to,
+                      'sex': sex
+                      }
         else:
             age = datetime.datetime.now() - datetime.timedelta(days=365 * int(age))
             age = age.year
-            params = {'fields': "first_name, bdate",
+            params = {'fields': "first_name, last_name, bdate, sex",
                       'q': city,
-                      'count': 1000,
+                      'count': 100,
                       'offset': 1,
                       'birth_year': age,
                       'sex': sex
@@ -74,16 +77,12 @@ class VK:
         res = requests.get(url=url, params={**self.params, **params}, headers=headers)
         result = res.json().get('response').get('items')
         with open('data.json', 'w') as file:
-            json.dump(res.json(), file, ensure_ascii=False, indent=3)
-        list_user, list_users = [], []
-        for item in result:
-            list_user.append(item.get('id'))
-            list_user.append(item.get('first_name'))
-            list_user.append(item.get('last_name'))
-            list_users.append(list_user)
-        return list_users
+            json.dump(result, file, ensure_ascii=False, indent=3)
+        return result
 
-    def get_users_photo(self, user_id):
+    def get_user_photo(self, user_id):
+        """ метод возвращает словарь вида {'href': [], 'owner_id': ""}
+        с ссылками на фото пользователя """
         url = "https://api.vk.com/method/photos.get"
         params = {
             'owner_id': user_id,
@@ -95,62 +94,115 @@ class VK:
         }
         headers = self.get_headers()
         res = requests.get(url=url, params={**self.params, **params}, headers=headers)
-        with open('photo.json', 'w') as file:
-            json.dump(res.json(), file, ensure_ascii=False, indent=3)
-        photos_info_list = res.json().get('response').get('items')
+        # with open('photo.json', 'w') as file:
+        #     json.dump(res.json(), file, ensure_ascii=False, indent=3)
+        photo_dict = res.json().get('response').get('items')
+
+        if len(photo_dict) < 3:
+            return None
+
         dict_likes = {'count': [], 'href': [], 'owner_id': ""}
         dict_likes_max = {'href': [], 'owner_id': ""}
-        if len(photos_info_list) != 0:
-            for photo in photos_info_list:
-                dict_likes['count'].append(photo.get('likes').get('count'))
-                dict_likes['href'].append(photo.get('sizes')[-1].get('url'))
-                dict_likes['owner_id'] = photo.get('owner_id')
+        for photo in photo_dict:
+            dict_likes['count'].append(photo.get('likes').get('count'))
+            dict_likes['href'].append(photo.get('sizes')[-1].get('url'))
+            dict_likes['owner_id'] = photo.get('owner_id')
+            if dict_likes.get('owner_id') != '':
+                while len(dict_likes_max.get("href")) <= 3:
 
-        if dict_likes.get('owner_id') != '':
-            while True:
-                if dict_likes.get('count') != []:
-                    max_like = max(dict_likes.get('count'))
-                    index = dict_likes.get('count').index(max_like)
-                else:
-                    index = 0
-                href = dict_likes.get('href')
-                if href != []:
-                    dict_likes.get('count').pop(index)
-                    max_href = href.pop(index)
-                    dict_likes_max['href'].append(max_href)
-                    dict_likes_max['owner_id'] = dict_likes.get('owner_id')
-                if len(dict_likes_max.get('href')) == 3 or len(dict_likes.get('href')) == 0:
-                    break
+                    if dict_likes.get('count') != []:
+                        dict_likes_max['owner_id'] = dict_likes.get('owner_id')
+                        max_like = max(dict_likes.get('count'))
+                        index = dict_likes.get('count').index(max_like)
+                    else:
+                        index = 0
+
+                    if dict_likes.get('href') != []:
+                        dict_likes.get('count').pop(index)
+                        dict_likes_max['href'].append(dict_likes.get('href').pop(index))
+
         return dict_likes_max
 
-    def users_info(self, city, sex, age):
+    # def getAll_user_photo(self, user_id):
+    #     """ Метод возвращает все фото пользователя """
+    #     url = "https://api.vk.com/method/photos.getAll"
+    #     params = {
+    #         'owner_id': user_id,
+    #         'extended': 1,
+    #         'photo_sizes': 1
+    #     }
+    #     headers = self.get_headers()
+    #     res = requests.get(url=url, params={**self.params, **params}, headers=headers)
+    #
+    #     with open('photo.json', 'w') as file:
+    #         json.dump(res.json(), file, ensure_ascii=False, indent=3)
+    #     photo_dict = res.json().get('response').get('items')
+    #
+    #     # Если кол-во фото пользователя < 3
+    #     if len(photo_dict) < 3:
+    #         return None
+    #     dict_likes = {'count': [], 'href': [], 'owner_id': ""}
+    #     dict_likes_max = {'href': [], 'owner_id': ""}
+    #
+    #     for photo in photo_dict:
+    #         dict_likes['count'].append(photo.get('likes').get('count'))
+    #         dict_likes['href'].append(photo.get('sizes')[-1].get('url'))
+    #         dict_likes['owner_id'] = photo.get('owner_id')
+    #
+    #     if dict_likes.get('owner_id') != '':
+    #         dict_likes_max['owner_id'] = dict_likes.get('owner_id')
+    #         while len(dict_likes_max.get('href')) <= 3:
+    #             # Ищем фото с макс. лайками
+    #             if dict_likes.get('count') != []:
+    #                 max_like = max(dict_likes.get('count'))
+    #                 index = dict_likes.get('count').index(max_like)
+    #             else:
+    #                 index = 0
+    #
+    #             if dict_likes.get('href') != []:
+    #                 dict_likes.get('count').pop(index)
+    #                 max_href = dict_likes.get('href').pop(index)
+    #                 dict_likes_max['href'].append(max_href)
+    #
+    #     return dict_likes_max
 
-        list_users = vk.get_users(city, sex, age)
-        list_new = []
-        for item in list_users:
-            time.sleep(3)
-            new_dict = {"link_photo": [], "user_name": "",  "user_link": ""}
-            dict1 = vk.get_users_photo(item[0])
-            if dict1.get("href") != []:
-                new_dict["link_photo"] = dict1.get("href")
-                new_dict["user_name"] = item[1] + item[2]
-                new_dict["user_link"] = "https://vk.com/id" + str(item[0])
-                list_new.append(new_dict)
-        return list_new
+    def user_info(self, dict_user_info, dict_user_photo):
+        new_dict = {"link_photo": [], "user_name": "", "user_link": ""}
+        for i in dict_user_photo.get("href"):
+            new_dict["link_photo"].append(i)
+        new_dict["user_name"] = dict_user_info.get("first_name") + " " + dict_user_info.get("last_name")
+        new_dict["user_link"] = "https://vk.com/id" + str(dict_user_info.get("id"))
+        return new_dict
 
 
 if __name__ == '__main__':
     access_token = os.getenv("access_token")
-    #для теста
-    list_input = ['30-40', 1, "Сочи"]
+    # для теста
+    list_input = ['30-40', 2, "Новосибирск"]
     age = list_input[0]
     city = list_input[2]
     sex = int(list_input[1])
     vk = VK(access_token)
-    user_info = vk.get_user(user_id)
-    # возвращает список словарей пользователей вида {"href": [], "first_name": "", "last_name": "", "user_link": ""}
-    pprint(vk.users_info(age, city, sex))
+    #получаем json пользователей отфильтрованных
+    users = vk.get_users(city, sex, age)
+    # В модуле бота прогонять пользователей через итератор
+    #iterator = iter(users[0])
+    list = []
+    # iterator = iter(users[0])
 
+    for i in users:
+        time.sleep(3)
+        pprint(i)
+        if not i.get("is_closed"):
+            f_user_id = i.get("id")
+            print(f_user_id)
+            user_photos = vk.get_user_photo(f_user_id)
+
+            if user_photos:
+                result = vk.user_info(i, user_photos)
+                print(f"список фото пользователя:{result}")
+                list.append(result)
+    pprint(list)
 
 
 
