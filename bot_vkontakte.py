bace4bd3e6fab23938f1f114087e0227e3821274
Remezov Dmitriy <darknessdizi@gmail.com>
2022-12-20 17:05:37 +0700

    Fixed a bug with the reset button while answering questions

--- bot_vkontakte.py
+++ bot_vkontakte.py
@@ -242,7 +242,13 @@ def event_handling_start(object_vk_api: object, message_text: str, variables: di
     sender_id = variables['id']
     variables = variables['fields']
     if message_text == 'сбросить':
-        variables['count'] = 0   # Найти баг!!! После сброса он никого не находит. !!!!!!!!!!!!!!!!
+        variables['count'] = 0
+        keyboard = create_buttons(2)
+        write_msg(object_vk_api, sender_id, bot_questions[variables['count']], keyboard)
+        variables['filtr_dict'] = {}
+        variables['count'] = 1
+        variables['continue'] = True
+        return variables   # Найти баг!!! После сброса он никого не находит. !!!!!!!!!!!!!!!!
     elif message_text == 'отменить':
         variables['count'] = 0
         variables['start'] = False
