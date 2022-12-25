f63f1676a5a2e839875945feb18fb5290fa359bb
Mariya Zamyatina <maria.net@mail.ru>
2022-12-25 17:13:04 +0700

    fix conflict

--- main.py
+++ main.py
@@ -1,137 +1,9 @@
-from base import *
+import base
 import bot_vkontakte as bot
 from token_vk import token_vk, sql_authorization
 from vk_api.longpoll import VkEventType
 from requests_to_vk import RequestsVk
-from VKinder import VKinder, PostgreSQL
 
-
-def photo_requests_for_users(
-    object_vk_api: object, response: object, 
-    cur: object, variables: dict) -> tuple:
-
-    '''Осуществляет запросы и поиск пользователей в Api вконтакте. 
-
-    Проверяет их на наличие в базе данных. Выводит фотографии 
-    
-    в чат-бота
-    
-    '''
-
-    respone = response.get_users(variables['fields']) 
-    block_list = [i[4] for i in base.get_favourites(cur, variables['id'], True)]
-    if respone is None:
-        bot.write_msg(
-            object_vk_api, variables['id'], 
-            "Ничего не найдено. Уточните параметры поиска"
-        )
-        variables['fields']['filtr_dict'] = {}
-        photos = None
-        return variables, respone, photos
-    elif len(respone) == 0:
-        bot.write_msg(object_vk_api, variables['id'], "Простите, людей не найдено")
-        variables['fields']['offset'] += 3
-        photos = None
-        return variables, respone, photos
-    else:
-        while True:
-            if respone[variables['fields']['number']][0] in block_list:
-                variables['fields']['number'] += 1
-                if len(respone) == variables['fields']['number']:
-                    bot.write_msg(
-                        object_vk_api, variables['id'], 
-                        "\U000026D4 Обновляю список обнаруженных людей. \U0001F605"
-                    )
-                    variables['fields']['number'] = 0
-                    variables['fields']['offset'] += 3
-                    respone = response.get_users(variables['fields'])
-                    keyboard = bot.create_buttons(4)
-                    bot.write_msg(
-                        object_vk_api, variables['id'], 
-                        "Подождите. Сейчас загружаю фотографии. \U0001F609", 
-                        keyboard
-                    )
-                    continue
-            else:
-                photos = response.get_users_photo(
-                    str(respone[variables['fields']['number']][0])
-                )
-                                    
-                if photos is None:
-                    keyboard = bot.create_buttons(1)
-                    bot.write_msg(
-                        object_vk_api, variables['id'], 
-                        "\U000026D4 \U0001F6AB У пользователя нет фотографий.\nНажмите следующий. \U0001F914", 
-                        keyboard
-                    )
-                    continue
-
-                text = respone[variables['fields']['number']][1]         
-                message = f"{text}\n https://vk.com/id{photos.get('owner_id')}"
-                bot.write_msg(object_vk_api, variables['id'], message) 
-
-                attachment = bot.add_photos(object_vk_api, photos.get('href')) 
-                bot.send_photos(object_vk_api, variables['id'], attachment)
-                keyboard = bot.create_buttons(4)
-                bot.write_msg(
-                    object_vk_api, variables['id'], 
-                    "Выполнено \U00002705", 
-                    keyboard
-                )
-                return variables, respone, photos
-
-
-def updates_the_list_of_people(
-    variables: dict, respone: dict, object_vk_api: object, 
-    response: object, cur: object) -> tuple:
-    
-
-    '''Обновляет список людей для вывода в чат бота'''
-
-    bot.write_msg(
-        object_vk_api, variables['id'], 
-        "\U000026D4 Обновляю список обнаруженных людей. \U0001F605"
-    )
-    variables['fields']['number'] = 0
-    variables['fields']['offset'] += 3
-    variables, respone, photos = photo_requests_for_users(
-        object_vk_api, response, cur, variables
-    )
-    return variables, respone, photos
-    
-
-def cancel_button(object_vk_api: object, variables: dict) -> dict:
-
-    '''Обновление переменных при нажатии кнопки "Отменить"
-    
-    '''
-
-    variables['count'] = 0
-    variables['start'] = False
-    variables['continue'] = False
-    variables['filtr_dict'] = {}
-    variables['fields']['number'] = 0
-    variables['fields']['offset'] = 0
-    bot.write_msg(
-        object_vk_api, variables['id'], 
-        "Желаете найти кого-то другого? \U0001F914 Наберите команду старт. \U0001F920"
-    )
-    return variables
-
-
-def save_to_favorites(cur: object, photos: list, 
-    respone: list, variables: dict) -> str:
-
-    id, name, bdate = respone[variables['fields']['number']] 
-    if not base.checking_list_favorites(cur, id):
-        sex = variables['fields']['filtr_dict'].get('sex')
-        city = variables['fields']['filtr_dict'].get('city')
-        link = f"https://vk.com/id{photos.get('owner_id')}"
-        favorites_id = base.add_favourites(
-            cur, id, name, bdate, sex, city, link
-        )
-        base.add_photos(cur, photos['href'], favorites_id)
-    return id
 
 
 def main():
@@ -149,7 +21,7 @@ def main():
     object_vkinder = bot.BotVkontakte(longpoll, session)
 
     # print(base.drop_table(cur)) #если нужно сбросить БД
-    print(create_db(cur))
+    print(base.create_db(cur))
 
     for event in longpoll.listen():
 
@@ -242,17 +114,17 @@ def main():
                                     break
 
                     elif message_text in ['добавить в избранное']:
-                        id = save_to_favorites(cur, photos, respone, variables)  
+                        id = bot.save_to_favorites(cur, photos, respone, variables)
                         if not base.checking_the_human_user_connection(
                             cur, variables['id'], id
                         ):
-                            add_a_human_user_relationship(
+                            base.add_a_human_user_relationship(
                                 cur, variables['id'], id, False
                             )
                         else:
                             if base.checking_the_human_user_connection(
-                                cur, variables['id'], id
-                            )[0][-1] == True: 
+                                    cur, variables['id'], id
+                            )[0][-1]:
                                 base.del_block_list(cur, variables['id'], id)
                             else:
                                 keyboard = bot.create_buttons(4)
@@ -263,17 +135,17 @@ def main():
                                 )
 
                     elif message_text in ['добавить в черный список']:
-                        id = save_to_favorites(cur, photos, respone, variables)
+                        id = bot.save_to_favorites(cur, photos, respone, variables)
                         if not base.checking_the_human_user_connection(
                             cur, variables['id'], id
                         ):
-                            add_a_human_user_relationship(
+                            base.add_a_human_user_relationship(
                                 cur, variables['id'], id, True
                             )
                         else:
-                            if base.checking_the_human_user_connection(
-                                cur, variables['id'], id
-                            )[0][-1] == False:
+                            if not base.checking_the_human_user_connection(
+                                    cur, variables['id'], id
+                            )[0][-1]:
                                 base.add_block_list(cur, variables['id'], id)
                             else:
                                 keyboard = bot.create_buttons(4)
